dc1c0841a7dfe402e5ffe9bac271888b0c2cc39a
pHenrymelo <pedrohenriquebmelo25@gmail.com>
2025-07-02 20:12:44 -0300

    fix: refactor code smell Long Method TimelineView habitDateViewAll using extract method refactoring technique

--- src/main/java/org/example/studyplanner/TimelineView.java
+++ src/main/java/org/example/studyplanner/TimelineView.java
@@ -9,17 +9,22 @@ public class TimelineView {
         List<Habit> habits = ht.getHabits();
         StringBuilder response = new StringBuilder();
         for(Habit habit : habits){
-            response.append("[ Habit: ")
-                    .append(habit.getName())
-                    .append(". Records: ");
-            List<LocalDateTime> records = ht.getHabitRecords(habit.getId());
-            for(LocalDateTime record : records){
-                response.append(ht.formatHabitDate(record)).append(", ");
-            }
-            response.append("]");
+            response.append(formatHabitWithRecords(ht, habit));
         }
-
         return response.toString();
+    }
+
+    private String formatHabitWithRecords(HabitTracker ht, Habit habit) {
+        StringBuilder result = new StringBuilder();
+        result.append("[ Habit: ")
+                .append(habit.getName())
+                .append(". Records: ");
+        List<LocalDateTime> records = ht.getHabitRecords(habit.getId());
+        for(LocalDateTime record : records){
+            result.append(ht.formatHabitDate(record)).append(", ");
+        }
+        result.append("]");
+        return result.toString();
     }
 
 }
